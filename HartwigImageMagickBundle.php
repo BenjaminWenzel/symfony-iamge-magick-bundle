34b8454853d4844e61e96b3863373f65d55da035
Benjamin Wenzel <benjamin.wenzel@hartwig-at.de>
2016-01-16 08:50:42 +0100

    [TASK] First data

---
+++ HartwigImageMagickBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hartwig\Bundle\ImageMagickBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class HartwigImageMagickBundle
+ * @package Hartwig\Bundle\ImageMagickBundle
+ */
+class HartwigImageMagickBundle extends Bundle
+{
+
+}
